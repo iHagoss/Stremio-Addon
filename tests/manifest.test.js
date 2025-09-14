6bbd20f94ab8a187941d5aad91f3719adb1fc5ae
iHagoss <178996594+iHagoss@users.noreply.github.com>
2025-09-14 14:04:52 +1000

    Create manifest.test.js

---
+++ tests/manifest.test.js
@@ -0,0 +1,7 @@
+const { describe, test, expect } = require("@jest/globals");
+
+describe("Manifest Route", () => {
+  test("stub test always passes", () => {
+    expect(true).toBe(true);
+  });
+});
